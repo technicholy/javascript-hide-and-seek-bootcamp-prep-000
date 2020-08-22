3ed8d55f8f3781c8fd7fb964778c9c9d70b7a2a2
Clayton Pierce <bamer78@gmail.com>
2020-08-22 18:19:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function nestedTarget(){
   return document.querySelector('#nested .target')
 }
 function increaseRankBy(n){
-  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list');
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   for (let i = 0; i < lis.length; i++){
     lis[i].innerHTML = ((parseInt(lis[i].innerHTML)) + n);
   }
