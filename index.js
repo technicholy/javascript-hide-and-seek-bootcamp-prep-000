0353d3117da2eb01f309c04c0845f32c03828be6
Clayton Pierce <bamer78@gmail.com>
2020-08-22 18:25:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function increaseRankBy(n){
   }
 }
 function deepestChild(){
-  return document.getElementById('grand-node').querySelectorAll('div')[2]
+  return document.getElementById('grand-node').querySelectorAll('div')[4]
 }
