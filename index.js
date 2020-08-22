8a2ea0927592a017966442534f01f333ed1ca375
Clayton Pierce <bamer78@gmail.com>
2020-08-22 18:01:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function nestedTarget(){
   return document.querySelector('#nested .target')
 }
 function increaseRankBy(n){
-  const lis = document.querySelectorAll('.ranked-list');
+  const lis = document.querySelectorAll('div.ranked-list');
   for (let i = 0; i < lis.length; i++){
     lis[i].innerHTML = (parseInt(i)+1).toString();
   }
